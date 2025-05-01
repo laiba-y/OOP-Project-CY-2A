82f2568e14f6ad86759e107e5a9c3a02143e400a
laiba-y <laibayousaf11@gmail.com>
2025-05-02 01:44:40 +0500

    Update employee.h

--- employee.h
+++ employee.h
@@ -1,1 +1,23 @@
+#pragma oncce
+#include<string>
+using namespace std;
+class Employee {
+protected:
+	string emp_id;
+	string pw;
+	char grade;
+	Message* msg;
+	Task* task;
+	PolicyEngine allowed;
+	Audit audit;
+
+public:
+	void sendmsg();
+	void receivemsg();
+	void CreateTask();
+	void delegate_task();
+	void Login();
+	string hashpw();
+	virtual void showMenu() = 0;
+};
 
