d24539fd82070a839d1c7760ed5845362869604a
aqsa-y <i242010@isb.nu.edu.pk>
2025-05-04 13:03:12 +0800

    Update manager.cpp

--- manager.cpp
+++ manager.cpp
@@ -1,1 +1,18 @@
-
+#include "manager.h"
+#include "classes.h"
+void managerfunc(string ID)
+{
+    Manager M;
+    M.showMenu(); //showing menu
+    int facility;
+    cin >> facility;
+    while (facility < 1 or facility > 3) //change numbers according to increase in facilities
+    {
+        cout << "Option not available, try again: ";
+        cin >> facility;
+    }
+    if (facility == 1)
+    {
+        M.CreateTask();
+    }
+}
