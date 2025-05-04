be5de278b21673156bf8d240e74be900f808f53b
aqsa-y <i242010@isb.nu.edu.pk>
2025-05-04 13:02:49 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,25 @@
+#include "junior.h"
+#include "manager.h"
+#include "director.h"
+#include "executive.h"
 
+int main()
+{
+    system ("clear");
+    int role;
+    cout << "Press 1 for junior, 2 for manager, 3 for director and 4 for executive: "; //add more options into this dont remove these lines as i have to code with the flow with these
+    cin >> role;
+    cin.ignore();
+    string ID;
+    cout << "Enter Registered ID: ";
+    getline(cin, ID);
+    // yahan par you add your login security checks and password stuff
+    if (role == 1)
+    {
+        juniorfunc(ID);
+    }
+    else if (role == 2);
+    {
+        managerfunc(ID);
+    }
+}
