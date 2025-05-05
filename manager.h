b040ad836d9a5781a6321c025d93e65f1a00942c
aqsa-y <i242010@isb.nu.edu.pk>
2025-05-06 01:12:36 +0800

    Update manager.h

--- manager.h
+++ manager.h
@@ -1,6 +1,18 @@
+#pragma once
 #include <iostream>
 #include <cstring>
 #include <fstream>
-
+#include "employee.h"
 using namespace std;
+class Manager : public Employee{
+    private:
+
+    public:
+    void showMenu()
+    {
+        cout << "------------------------ MENU --------------------------------";
+        cout << "\nPRess 1 for assigning tasks, 2 for viewing tasks, 3 for delegating tasks, 4 for sending messages, 5 for recieving messages, 6 to check Perfomance Reports, 7 to Exit : \n";  // I am making a very rough sa main right now, jab sab hojayega phir I will add decorations and shit
+    }
+};
+
 void managerfunc(string ID);
