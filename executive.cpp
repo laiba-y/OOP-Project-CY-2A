332cba9e9004a4f1edade4d0045aad9bb490cc4d
aqsa-y <i242010@isb.nu.edu.pk>
2025-05-06 01:10:30 +0800

    Update executive.cpp

--- executive.cpp
+++ executive.cpp
@@ -1,1 +1,27 @@
-
+#include "executive.h"
+#include "classes.h"
+#include "employee.h"
+void executivefunc(string ID)
+{
+    Executive Ex;
+    Ex.showMenu(); //showing menu
+    int facility;
+    cin >> facility;
+    while (facility < 1 or facility > 3) //change numbers according to increase in facilities
+    {
+        cout << "Option not available, try again: ";
+        cin >> facility;
+    }
+    if (facility == 1)
+    {
+        Ex.CreateTask();
+    }
+    else if (facility == 2)
+    {
+        Ex.displayTask();
+    }
+    else if (facility == 3)
+    {
+        Ex.delegate_task();
+    }
+}
