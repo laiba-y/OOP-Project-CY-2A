252bd8b972849886073711fc80e87c6f805dcb10
aqsa-y <i242010@isb.nu.edu.pk>
2025-05-06 01:12:16 +0800

    Update manager.cpp

--- manager.cpp
+++ manager.cpp
@@ -1,12 +1,13 @@
 #include "manager.h"
 #include "classes.h"
+#include "employee.h"
 void managerfunc(string ID)
 {
     Manager M;
     M.showMenu(); //showing menu
     int facility;
     cin >> facility;
-    while (facility < 1 or facility > 3) //change numbers according to increase in facilities
+    while (facility < 1 or facility > 7) //change numbers according to increase in facilities
     {
         cout << "Option not available, try again: ";
         cin >> facility;
@@ -14,5 +15,47 @@ void managerfunc(string ID)
     if (facility == 1)
     {
         M.CreateTask();
+    }
+    else if (facility == 2)
+    {
+        M.displayTask();
+    }
+    else if (facility == 3)
+    {
+        M.delegate_task();
+    }
+    else if (facility == 4)
+    {
+        M.sendmsg();
+    }
+    else if (facility == 5)
+    {
+        M.receivemsg();
+    }
+    else if (facility == 6)
+    {
+        //performance reports here
+    }
+    else
+    {
+        cout << "--------------------------- LOGGING OUT ------------------------------------" << endl;
+        return;
+    }
+    int option;
+    cout << "Press 3 to Continue, 4 to Exit : ";
+    cin >> option;
+
+    while (option < 3 or option > 4)
+    {
+        cout << "Invalid option, Try again: ";
+        cin >> option;
+    }
+    if (option == 3)
+    {
+        managerfunc(ID);
+    }
+    else {
+        cout << "----------------------------- LOGGING OUT ----------------------------" << endl;
+        return;
     }
 }
