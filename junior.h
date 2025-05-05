85e6a41eaf7024a9543b9be8638e8f08f85eb061
aqsa-y <i242010@isb.nu.edu.pk>
2025-05-06 01:11:43 +0800

    Update junior.h

--- junior.h
+++ junior.h
@@ -1,1 +1,17 @@
+#pragma once
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include "employee.h"
+using namespace std;
+class Junior : public Employee{
+    private:
 
+    public:
+    void showMenu()
+    {
+        cout << "------------------------ MENU --------------------------------";
+        cout << "\nPRess 1 for viewing tasks, 2 for delegating tasks, 3 sending messages, 4 viewing messages, 5 viewing your perfomance report, 6 to exit : \n";  // I am making a very rough sa main right now, jab sab hojayega phir I will add decorations and shit
+    }
+};
+void juniorfunc(string ID);
