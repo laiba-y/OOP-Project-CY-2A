0e5d94c634752c5be6d0079268183cb1ba35392f
aqsa-y <i242010@isb.nu.edu.pk>
2025-05-06 01:09:43 +0800

    Update director.h

--- director.h
+++ director.h
@@ -1,1 +1,17 @@
+#pragma once
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include "employee.h"
+#include "classes.h"
+#include "executive.h"
+using namespace std;
+class Director: public HigherUp{
+    public:
+    void showmenu()
+    {
+       cout << "Press blah blah" << endl;
+    }
+};
+void directorfunc(string ID);
 
