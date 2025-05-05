da5225d8ce5552ab5ab3c2c1c1304282fbbca7e3
aqsa-y <i242010@isb.nu.edu.pk>
2025-05-06 01:10:03 +0800

    Update employee.h

--- employee.h
+++ employee.h
@@ -1,5 +1,8 @@
-#pragma oncce
+#pragma once
+#include <iostream>
 #include<string>
+#include <fstream>
+#include "classes.h"
 using namespace std;
 class Employee {
     protected:
@@ -12,8 +15,10 @@ class Employee {
     Audit audit;
     
     public:
-    void sendmsg();
-    void receivemsg();
+    void sendmsg()
+    {}
+    void receivemsg()
+    {}
     void CreateTask()
     {
         
@@ -27,7 +32,23 @@ class Employee {
             task->appendtask("M5");
         }
     }
-    void delegate_task(){}
+    void delegate_task()
+    {
+        grade = 'M'; // for my ease, iski assignment you will do once you update the data in users.txt
+        char target;
+        cout << "Enter the grade of the person you want to Delegate a Task to: ";
+        cin >> target;
+        if (allowed.isAllowed(grade, target))
+        {
+            cout << "Access Permitted! \n";
+            if (!task)
+            {
+                delete task; //recheck any memory issue here
+            }
+            task = new Task; //dynamically allocating space for a new task
+            task->delegateTask("M2");
+        }
+    }
     void displayTask()
     {
         if (!task)
@@ -35,5 +56,13 @@ class Employee {
             delete task; //recheck any memory issue here
         }
         task = new Task;
-         task->displayTask("J4");
+         task->displayTask("J6");
     }
+    void Login();
+    string hashpw();
+    virtual void showMenu() = 0;
+    ~Employee()
+    {
+        delete task;
+    }
+    };
