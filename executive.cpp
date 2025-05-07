fed9ddcb700b445f59049ab345d09d5ccbe4bcf5
aqsa-y <i242010@isb.nu.edu.pk>
2025-05-08 00:26:01 +0800

    Update executive.cpp

--- executive.cpp
+++ executive.cpp
@@ -3,15 +3,20 @@
 #include "employee.h"
 void executivefunc(string ID)
 {
-    Executive Ex;
+    bool running = true;
+    Executive Ex(ID);
+    while (running)
+    {
     Ex.showMenu(); //showing menu
     int facility;
     cin >> facility;
-    while (facility < 1 or facility > 3) //change numbers according to increase in facilities
+    while (facility < 1 or facility > 8) //change numbers according to increase in facilities
     {
         cout << "Option not available, try again: ";
         cin >> facility;
     }
+    while (running)
+    
     if (facility == 1)
     {
         Ex.CreateTask();
@@ -24,4 +29,42 @@ void executivefunc(string ID)
     {
         Ex.delegate_task();
     }
+    else if (facility == 4)
+    {
+        Ex.sendmsg();
+    }
+    else if (facility == 5)
+    {
+        Ex.receivemsg();
+    }
+    else if (facility == 6)
+    {
+        Ex.sendGlobalMsg();
+    }
+    else if (facility == 7)
+    {
+        string id;
+        cout << "Enter the desired ID : ";
+        getline(cin, id);
+        Ex.showperformance(id);
+    }
+    else if (facility == 8)
+    {
+        cout << "------------------ LOGGING OUT ---------------------------";
+        return;
+    }
+    int option;
+    cout << "Press 3 to Continue, 4 to Exit: ";
+    cin >> option;
+
+    while (option < 3 || option > 4) {
+        cout << "Invalid option, Try again: ";
+        cin >> option;
+    }
+
+    if (option == 4) {
+        cout << "----------------------------- LOGGING OUT ----------------------------" << endl;
+        running = false;
+    }
+}
 }
