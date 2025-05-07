321b49e802226f65deb8f395e4679c5010045e15
aqsa-y <i242010@isb.nu.edu.pk>
2025-05-08 00:19:08 +0800

    Update employee.h

--- employee.h
+++ employee.h
@@ -1,28 +1,64 @@
 #pragma once
 #include <iostream>
-#include<string>
+#include <string>
 #include <fstream>
 #include "classes.h"
 using namespace std;
-class Employee {
-    protected:
+
+class Employee
+{
+protected:
     string emp_id;
     string pw;
     int grade;
-    Message* msg;
-    Task* task;
+    Message *msg;
+    Task *task;
     PolicyEngine allowed;
     Audit audit;
-    
-    public:
-    Employee(){
+
+public:
+    Employee()
+    {
         task = new Task;
         msg = new Message;
         cout << "Constructor called " << endl;
     }
+    bool IDexists(string emp_id)
+    {
+        string id, password;
+        bool found = false;
+        ifstream check;
+        check.open("users.txt");
+        while (getline(check, id, ' '))
+        {
+            getline(check, password, '\n');
+            if (id == emp_id)
+            {
+            found = true;
+            break;
+            }
+        }
+        return found;
+    }
+    void showperformance(string emp_id)
+    {
+        cout << "---------------- REPORT " << emp_id << " -----------------------" << endl;
+        cout << " DEFAULT SCORE : 100" << endl;
+        cout << "Marking criteria: \nComplete Task +10 \nFail Task -10\nDelegate Task +5\n";
+        string id;
+        int score;
+        ifstream in;
+        in.open("reports.txt");
+        while (getline(in, id, ' ') && (in >> score))
+        {
+             if (id == emp_id)
+             {
+                cout << "Obtained score : " << score << endl;
+             }
+        }
+    }
     void sendmsg()
     {
-        grade = 1;
         int msgtostring;
         string msgtype;
         string T_id;
@@ -32,6 +68,11 @@ class Employee {
         cout << "Enter ID of recipient: ";
         cin.ignore();
         getline(cin, T_id);
+        if (!IDexists(T_id))
+        {
+            cout << "ID does not exist, please choose another user: ";
+            getline(cin, T_id);
+        }
         cout << "Press 1 for PRIVATE, 2 for INFO, 3 for ALERT, 4 for WARNING : ";
         cin >> msgtostring;
 
@@ -40,76 +81,111 @@ class Employee {
             cout << "Invalid Option, Try again: ";
             cin >> msgtostring;
         }
-        
+
         if (msgtostring == 1)
-        msgtype = "PRIVATE";
+            msgtype = "PRIVATE";
         else if (msgtostring == 2)
-        msgtype = "INFO";
+            msgtype = "INFO";
         else if (msgtostring == 3)
-        msgtype = "ALERT";
+            msgtype = "ALERT";
         else
-        msgtype = "WARNING";
+            msgtype = "WARNING";
 
         if (allowed.canSendMessage(grade, Tgrade, msgtype))
         {
-              msg->sendmsg(emp_id, T_id, msgtype);
+            msg->sendmsg(emp_id, T_id, msgtype);
         }
-        
+        else
+        {
+            cout << "Permission denied! " << endl;
+        }
     }
     void receivemsg()
-    {}
+    {
+        if (!msg)
+        {
+            delete msg;
+            msg = new Message;
+        }
+        msg->receivemsg(emp_id);
+    }
     void CreateTask()
     {
-        
-        grade = 1;// for my ease, iski assignment you will do once you update the data in users.txt
-        char target;
+        string T_ID;
+        int target;
         cout << "Enter the grade of the person you want to Assign a Task to: ";
         cin >> target;
+        while (target < 1 or target > 4)
+        {
+            cout << "Grade doesnt exist, Try again: ";
+            cin >> target;
+        }
         if (allowed.can_assign(grade, target))
         {
+            cout << "Access permitted!\nEnter the Receiver's ID: ";
+            cin.ignore();
+            std::getline(cin, T_ID);
+            if (!IDexists(T_ID))
+        {
+            cout << "ID does not exist, please choose another user: ";
+            getline(cin, T_ID);
+        }
             if (!task)
-            task = new Task; //dynamically allocating space for a new task
-            task->appendtask(emp_id);
+                task = new Task; // dynamically allocating space for a new task
+            task->appendtask(emp_id, T_ID);
+        }
+        else
+        {
+            cout << "Permission denied! " << endl;
         }
     }
     void delegate_task()
     {
-        grade = 1;; // for my ease, iski assignment you will do once you update the data in users.txt
+        string T_ID;
         int target;
         cout << "Enter the grade of the person you want to Delegate a Task to: ";
         cin >> target;
-        if (allowed.can_delegate(grade, target))
+
+        while (target < 1 or target > 4)
         {
+            cout << "Grade doesnt exist, Try again: ";
+            cin >> target;
+        }
+
+        if (allowed.can_delegate(grade, target))
+        { 
             cout << "Access Permitted! \n";
             if (!task)
             {
-                delete task; //recheck any memory issue here
+                delete task;
             }
-            task = new Task; //dynamically allocating space for a new task
+            task = new Task; // dynamically allocating space for a new task
             task->delegateTask(emp_id);
+        }
+        else
+        {
+            cout << "Permission denied! " << endl;
         }
     }
     void displayTask()
     {
         if (!task)
         {
-            delete task; //recheck any memory issue here
+            delete task;
         }
         task = new Task;
-         task->displayTask(emp_id);
+        task->displayTask(emp_id);
     }
-    void Login();
-    string hashpw();
     virtual void showMenu() = 0;
     ~Employee()
     {
         if (!task)
         {
-            delete task; 
+            delete task;
         }
         if (!msg)
         {
             delete msg;
         }
     }
-    };
+};
