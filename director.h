52fd03585de4df43c6cff7bbca640b4499904d91
aqsa-y <i242010@isb.nu.edu.pk>
2025-05-08 00:26:22 +0800

    Update director.h

--- director.h
+++ director.h
@@ -8,10 +8,15 @@
 using namespace std;
 class Director: public HigherUp{
     public:
+    Director(string id)
+    {
+        emp_id = id;
+        grade = 3;
+    }
     void showmenu()
     {
-       cout << "Press blah blah" << endl;
+       cout << "Press 1 for assigning task, 2 for viewing task, 3 for delegating task, 4 for sending messages, 5 for recieving messages ";
+       cout << "6 to Send global messages, 7 for performance reports , 8 to exit " << endl;
     }
 };
 void directorfunc(string ID);
-
