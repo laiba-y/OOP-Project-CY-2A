b96c071311c51c92a08bcc14380d5999b164ffb2
aqsa-y <i242010@isb.nu.edu.pk>
2025-05-07 15:26:13 +0800

    Update junior.cpp

--- junior.cpp
+++ junior.cpp
@@ -1,56 +1,58 @@
 #include "junior.h"
 #include "classes.h"
-void juniorfunc(string ID)
-{
-    Junior J;
-    J.showMenu();
-    int facility;
-    cin >> facility;
-    while (facility < 1 or facility > 6) //change numbers according to increase in facilities
-    {
-        cout << "Option not available, try again: ";
-        cin >> facility;
-    }
-    if (facility == 1)
-    {
-        J.displayTask();       
-    }
-    else if (facility == 2)
-    {
-        J.delegate_task();       
-    }
-    else if (facility == 3)
-    {
-        J.sendmsg();
-    }
-    else if (facility == 4)
-    {
-        J.receivemsg();
-    }
-    else if (facility == 5)
-    {
-        //show performance report here
-    }
-    else 
-    {
-        cout << "----------------------------------- LOGGING OUT ----------------------------------------" << endl;
-        return;
-    }
-    int option;
-    cout << "Press 3 to Continue, 4 to Exit : ";
-    cin >> option;
 
-    while (option < 3 or option > 4)
-    {
-        cout << "Invalid option, Try again: ";
+void juniorfunc(string ID) {
+    Junior J(ID);
+    bool running = true;
+
+    while (running) {
+        J.showMenu();
+        int facility;
+        cin >> facility;
+
+        while (facility < 1 || facility > 6) {
+            cout << "Option not available, try again: ";
+            cin >> facility;
+        }
+
+        if (facility == 1)
+        {
+            J.displayTask();       
+        }
+        else if (facility == 2)
+        {
+            J.delegate_task();       
+        }
+        else if (facility == 3)
+        {
+            J.sendmsg();
+        }
+        else if (facility == 4)
+        {
+            J.receivemsg();
+        }
+        else if (facility == 5)
+        {
+            //show performance report here
+        }
+        else 
+        {
+            cout << "----------------------------------- LOGGING OUT ----------------------------------------" << endl;
+            return;
+        }
+
+        int option;
+        cout << "Press 3 to Continue, 4 to Exit: ";
         cin >> option;
-    }
-    if (option == 3)
-    {
-        juniorfunc(ID);
-    }
-    else {
-        cout << "----------------------------- LOGGING OUT ----------------------------" << endl;
-        return;
+
+        while (option < 3 || option > 4) {
+            cout << "Invalid option, Try again: ";
+            cin >> option;
+        }
+
+        if (option == 4) {
+            cout << "----------------------------- LOGGING OUT ----------------------------" << endl;
+            running = false;
+        }
     }
 }
