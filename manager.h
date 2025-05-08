a20ea284858c5c8a100c2ff3ad265427d1cadfb2
aqsa-y <i242010@isb.nu.edu.pk>
2025-05-09 01:14:51 +0800

    Update manager.h

--- manager.h
+++ manager.h
@@ -11,12 +11,14 @@ class Manager : public Employee{
     Manager(string id)
     {
         emp_id = id;
-        grade = 2;
+        grade = 2; //clearance level
     }
     void showMenu()
     {
-        cout << "------------------------ MENU --------------------------------";
-        cout << "\nPRess 1 for assigning tasks, 2 for viewing tasks, 3 for delegating tasks, 4 for sending messages, 5 for recieving messages, 6 to check Perfomance Reports, 7 to Exit : \n";  // I am making a very rough sa main right now, jab sab hojayega phir I will add decorations and shit
+        cout << "-------------------------------------------------- DASH BOARD -------------------------------------------------------------";
+        cout << "\nPress \n1. ASSIGN TASKS \n2. VIEW TASKS \n3. DELEGATE TASKS \n4. SEND MESSAGES \n5. VIEW MESSAGES \n6. CHECK PERFORMANCE REPORT \n7. EXIT : \n"; 
+        // cout << "------------------------ MENU --------------------------------";
+        // cout << "\nPRess 1 for assigning tasks, 2 for viewing tasks, 3 for delegating tasks, 4 for sending messages, 5 for recieving messages, 6 to check Perfomance Reports, 7 to Exit : \n";  // I am making a very rough sa main right now, jab sab hojayega phir I will add decorations and shit
     }
 };
 void managerfunc(string ID);
