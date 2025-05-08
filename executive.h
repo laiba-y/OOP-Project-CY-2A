cf517f30926754f9cb3a503c86f614dda6ae06f5
aqsa-y <i242010@isb.nu.edu.pk>
2025-05-09 01:13:10 +0800

    Update executive.h

--- executive.h
+++ executive.h
@@ -9,38 +9,46 @@ class HigherUp : public Employee{
     private:
     Global global;
     Login log;
+    Tracker report;
     public: 
     HigherUp()
     {
 
     }
-    void adduser()
+    void adduser(string emp_id)
     {
+        report.setid(emp_id);
+        report += 20;
         string ID, pw;
-        cout << "Enter ID for the User: ";
+        cout << "Enter ID for NEW USER: ";
         getline(cin, ID);
         if (IDexists(ID))
         {
             cout << "ID already exists! Choose another ID: ";
             getline(cin, ID);
         }
-        cout << "Choose password for the user: ";
+        cout << "Enter PASSWORD for NEW user: ";
         getline(cin, pw);
         pw = log.hashpw(pw);
         fstream add;
         add.open("users.txt", ios::app);
         add << ID << " " << pw << endl;
         add.close();
+        cout << ID << " HIRED!" << endl;
     }
     void showMenu()
     {
-        cout << "Press 1 for assigning task, 2 for viewing task, 3 for delegating task, 4 for sending messages, 5 for recieving messages ";
-       cout << "6 to Send global messages, 7 for performance reports , 8 to Hire a user, 9 to Exit " << endl;
+        cout << "-------------------------------------------------- DASH BOARD -------------------------------------------------------------";
+        cout << "\nPress \n1. ASSIGN TASKS \n2. VIEW TASKS \n3. DELEGATE TASK \n4. SEND MESSAGES \n5. VIEW MESSAGES \n6. SEND GLOBAL NOTIFICATION \n7. VIEW PERFORMANCE REPORTS \n8. HIRE A NEW USER \n9. EXIT \nENTER HERE: \n"; 
+    //     cout << "Press 1 for assigning task, 2 for viewing task, 3 for delegating task, 4 for sending messages, 5 for recieving messages ";
+    //    cout << "6 to Send global messages, 7 for performance reports , 8 to Hire a user, 9 to Exit " << endl;
     }
-    void sendGlobalMsg()
+    void sendGlobalMsg(string emp_id)
     {
+        report.setid(emp_id);
+        report += 3;
         int msgtype;
-        cout << "Press \n1 to Send Alert, 2 to Send Emergency Message, 3 to Send Warning, 4 to Exit: ";
+        cout << "Press \n1. ALERT  \n2. EMERGENCY \n3. WARNING \n4. EXIT \nENTER HERE : ";
         cin >> msgtype;
         cin.ignore();
         while (msgtype < 1 or msgtype > 4)
@@ -75,7 +83,6 @@ class Executive: public HigherUp{
     }
     void showmenu()
     {
-        cout << "Press blah blah" << endl;   
     }
 };
 void executivefunc(string ID);
