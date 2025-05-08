51d89b54db593269d765add429703169525a889e
aqsa-y <i242010@isb.nu.edu.pk>
2025-05-09 01:13:43 +0800

    Update junior.h

--- junior.h
+++ junior.h
@@ -4,18 +4,19 @@
 #include <fstream>
 #include "employee.h"
 using namespace std;
-class Junior : public Employee{
-    private:
-
-    public:
-    Junior(string id){
+class Junior : public Employee
+{
+private:
+public:
+    Junior(string id)
+    {
         emp_id = id;
         grade = 1;
     }
     void showMenu()
     {
-        cout << "------------------------ MENU --------------------------------";
-        cout << "\nPRess 1 for viewing tasks, 2 for delegating tasks, 3 sending messages, 4 viewing messages, 5 viewing your perfomance report, 6 to exit : \n";  // I am making a very rough sa main right now, jab sab hojayega phir I will add decorations and shit
+        cout << "-------------------------------------------------- DASH BOARD -------------------------------------------------------------";
+        cout << "\nPress \n1. VIEW TASKS \n2. DELEGATE TASKS \n3. SEND MESSAGE \n4. VIEW MESSAGES \n5. VIEW YOUR PERFORMANCE REPORT \n6. EXIT : \n";
     }
 };
 void juniorfunc(string ID);
