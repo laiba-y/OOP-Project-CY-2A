560952f278a4015c08f9e9bc0b2f9c3dbe161d8f
aqsa-y <i242010@isb.nu.edu.pk>
2025-05-10 00:38:42 +0800

    Update junior.cpp

--- junior.cpp
+++ junior.cpp
@@ -1,27 +1,30 @@
 #include "junior.h"
 #include "classes.h"
 
-void juniorfunc(string ID) {
+void juniorfunc(string ID)
+{
     Junior J(ID);
     bool running = true;
-    
-    while (running) {
+
+    while (running)
+    {
         J.showMenu();
         int facility;
         cin >> facility;
         system("clear");
-        while (facility < 1 || facility > 6) {
+        while (facility < 1 || facility > 6)
+        {
             cout << "Option not available, try again: ";
             cin >> facility;
         }
 
         if (facility == 1)
         {
-            J.displayTask();       
+            J.displayTask();
         }
         else if (facility == 2)
         {
-            J.delegate_task();       
+            J.delegate_task();
         }
         else if (facility == 3)
         {
@@ -36,33 +39,37 @@ void juniorfunc(string ID) {
             J.showperformance(ID);
         }
         else
-    {
-        cout << GREEN;
-        cout << "╔════════════════════════════════════════════════════════════╗\n";
-        cout << "║                       LOGGING OUT...                       ║\n";
-        cout << "╚════════════════════════════════════════════════════════════╝\n";
+        {
+            Audit::logAction(ID, "LOG OUT", "SUCCESS");
+            cout << GREEN;
+            cout << "╔════════════════════════════════════════════════════════════╗\n";
+            cout << "║                       LOGGING OUT...                       ║\n";
+            cout << "╚════════════════════════════════════════════════════════════╝\n";
+            cout << RESET;
+            return;
+        }
+        int option;
+        cout << BCYAN;
+        cout << "╔══════════════════════════════════════╗\n";
+        cout << "║              PRESS                   ║\n";
+        cout << "╠══════════════════════════════════════╣\n";
+        cout << "║ 1) DASH BOARD                        ║\n";
+        cout << "║ 2) LOG OUT                           ║\n";
+        cout << "╚══════════════════════════════════════╝\n";
         cout << RESET;
-        return;
-    }
-    int option;
-    cout << BCYAN;
-    cout << "╔══════════════════════════════════════╗\n";
-    cout << "║              PRESS                   ║\n";
-    cout << "╠══════════════════════════════════════╣\n";
-    cout << "║ 1) DASH BOARD                        ║\n";
-    cout << "║ 2) LOG OUT                           ║\n";
-    cout << "╚══════════════════════════════════════╝\n";
-    cout << RESET;
 
-    cout << YELLOW << "ENTER HERE: ";
+        cout << YELLOW << "ENTER HERE: ";
         cin >> option;
-cout << RESET;
-        while (option < 1 || option > 2) {
+        cout << RESET;
+        while (option < 1 || option > 2)
+        {
             cout << "Invalid option, Try again: ";
             cin >> option;
         }
-
-        if (option == 2) {
+        system("clear");
+        if (option == 2)
+        {
+            Audit::logAction(ID, "LOG OUT", "SUCCESS");
             cout << GREEN;
             cout << "╔════════════════════════════════════════════════════════════╗\n";
             cout << "║                       LOGGING OUT...                       ║\n";
