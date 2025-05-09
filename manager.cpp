f9979992f6b4e07e541cf4d5433f722e57e228cc
aqsa-y <i242010@isb.nu.edu.pk>
2025-05-10 00:39:49 +0800

    Update manager.cpp

--- manager.cpp
+++ manager.cpp
@@ -7,67 +7,71 @@ void managerfunc(string ID)
     bool running = true;
     while (running)
     {
-    M.showMenu(); //showing menu
-    int facility;
-    cin >> facility;
-    while (facility < 1 or facility > 7) //change numbers according to increase in facilities
-    {
-        cout << "Option not available, try again: ";
+        M.showMenu(); // showing menu
+        int facility;
         cin >> facility;
-    }
-    system("clear");
-    if (facility == 1)
-    {
-        M.CreateTask();
-    }
-    else if (facility == 2)
-    {
-        M.displayTask();
-    }
-    else if (facility == 3)
-    {
-        M.delegate_task();
-    }
-    else if (facility == 4)
-    {
-        M.sendmsg();
-    }
-    else if (facility == 5)
-    {
-        M.receivemsg();
-    }
-    else if (facility == 6)
-    {
-        M.showperformance(ID);
-    }
-    else
-    {
-        cout << GREEN;
-        cout << "╔════════════════════════════════════════════════════════════╗\n";
-        cout << "║                       LOGGING OUT...                       ║\n";
-        cout << "╚════════════════════════════════════════════════════════════╝\n";
+        while (facility < 1 or facility > 7) // change numbers according to increase in facilities
+        {
+            cout << "Option not available, try again: ";
+            cin >> facility;
+        }
+        system("clear");
+        if (facility == 1)
+        {
+            M.CreateTask();
+        }
+        else if (facility == 2)
+        {
+            M.displayTask();
+        }
+        else if (facility == 3)
+        {
+            M.delegate_task();
+        }
+        else if (facility == 4)
+        {
+            M.sendmsg();
+        }
+        else if (facility == 5)
+        {
+            M.receivemsg();
+        }
+        else if (facility == 6)
+        {
+            M.showperformance(ID);
+        }
+        else
+        {
+            Audit::logAction(ID, "LOG OUT", "SUCCESS");
+            cout << GREEN;
+            cout << "╔════════════════════════════════════════════════════════════╗\n";
+            cout << "║                       LOGGING OUT...                       ║\n";
+            cout << "╚════════════════════════════════════════════════════════════╝\n";
+            cout << RESET;
+            return;
+        }
+        int option;
+        cout << BCYAN;
+        cout << "╔══════════════════════════════════════╗\n";
+        cout << "║              PRESS                   ║\n";
+        cout << "╠══════════════════════════════════════╣\n";
+        cout << "║ 1) DASH BOARD                        ║\n";
+        cout << "║ 2) LOG OUT                           ║\n";
+        cout << "╚══════════════════════════════════════╝\n";
         cout << RESET;
-        return;
-    }
-    int option;
-    cout << BCYAN;
-    cout << "╔══════════════════════════════════════╗\n";
-    cout << "║              PRESS                   ║\n";
-    cout << "╠══════════════════════════════════════╣\n";
-    cout << "║ 1) DASH BOARD                        ║\n";
-    cout << "║ 2) LOG OUT                           ║\n";
-    cout << "╚══════════════════════════════════════╝\n";
-    cout << RESET;
 
-    cout << YELLOW << "ENTER HERE: ";
+        cout << YELLOW << "ENTER HERE: ";
         cin >> option;
-cout << RESET;
-        while (option < 1 || option > 2) {
+        cout << RESET;
+        while (option < 1 || option > 2)
+        {
             cout << "Invalid option, Try again: ";
             cin >> option;
         }
-
-        if (option == 2) {
+        system("clear");
+        if (option == 2)
+        {
+            Audit::logAction(ID, "LOG OUT", "SUCCESS");
             cout << GREEN;
             cout << "╔════════════════════════════════════════════════════════════╗\n";
             cout << "║                       LOGGING OUT...                       ║\n";
@@ -75,5 +79,5 @@ cout << RESET;
             cout << RESET;
             running = false;
         }
-}
+    }
 }
