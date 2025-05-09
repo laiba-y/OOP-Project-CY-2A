4e105d5bce08bc575d0997715512cced293a631c
aqsa-y <i242010@isb.nu.edu.pk>
2025-05-09 19:44:07 +0800

    Update manager.cpp

--- manager.cpp
+++ manager.cpp
@@ -15,6 +15,7 @@ void managerfunc(string ID)
         cout << "Option not available, try again: ";
         cin >> facility;
     }
+    system("clear");
     if (facility == 1)
     {
         M.CreateTask();
@@ -41,20 +42,37 @@ void managerfunc(string ID)
     }
     else
     {
-        cout << "#################################### LOGGING OUT #######################################" << endl;
+        cout << GREEN;
+        cout << "╔════════════════════════════════════════════════════════════╗\n";
+        cout << "║                       LOGGING OUT...                       ║\n";
+        cout << "╚════════════════════════════════════════════════════════════╝\n";
+        cout << RESET;
         return;
     }
     int option;
-        cout << "Press \n1) HOME PAGE \n2) LOG OUT \nENTER HERE: ";
-        cin >> option;
+    cout << BCYAN;
+    cout << "╔══════════════════════════════════════╗\n";
+    cout << "║              PRESS                   ║\n";
+    cout << "╠══════════════════════════════════════╣\n";
+    cout << "║ 1) DASH BOARD                        ║\n";
+    cout << "║ 2) LOG OUT                           ║\n";
+    cout << "╚══════════════════════════════════════╝\n";
+    cout << RESET;
 
-        while (option < 3 || option > 4) {
+    cout << YELLOW << "ENTER HERE: ";
+        cin >> option;
+cout << RESET;
+        while (option < 1 || option > 2) {
             cout << "Invalid option, Try again: ";
             cin >> option;
         }
 
-        if (option == 4) {
-            cout << "#################################### LOGGING OUT #######################################" << endl;
+        if (option == 2) {
+            cout << GREEN;
+            cout << "╔════════════════════════════════════════════════════════════╗\n";
+            cout << "║                       LOGGING OUT...                       ║\n";
+            cout << "╚════════════════════════════════════════════════════════════╝\n";
+            cout << RESET;
             running = false;
         }
 }
